ef8b762bfcc5714cfdb6f45d80432ef1a55265de
zachtownleysmith <zachtownleysmith@yahoo.com>
2021-05-10 22:58:02 -0500

    Added Article count as a feature and error checking

--- flask_app.py
+++ flask_app.py
@@ -4,23 +4,32 @@ from get_news import bing_api_search
 app = Flask(__name__)
 
 
-#@app.route("/output", methods=["POST","GET"])
-#def served_articles(data):
-    #return render_template("TB_Senti.html", data=data)
-
-
 @app.route("/getSentiment", methods=["POST", "GET"])
 def splash():
     if request.method == "POST":
         company = request.form["company_test"]
         sentiment = request.form["sentiment_test"]
-        data = bing_api_search(company)
+
+        # Add Error Check for non-integer article number POST, default 5
+        try:
+            article_count = int(request.form["article_cnt"])
+            bad_count = None
+            proper_count = True
+        except ValueError:
+            article_count = 5
+            bad_count = request.form["article_cnt"]
+            proper_count = False
+
+        data = bing_api_search(search_term=company, search_num=article_count)
         if sentiment == "Aggregated":
-            return render_template("Multiple_Senti.html", data=data, sentiment=sentiment)
+            return render_template("Multiple_Senti.html", data=data, sentiment=sentiment,
+                                   article_count=article_count, proper_count=proper_count, bad_count=bad_count)
         elif sentiment == "TextBlob":
-            return render_template("TB_Senti.html", data=data, sentiment=sentiment)
+            return render_template("TB_Senti.html", data=data, sentiment=sentiment,
+                                   article_count=article_count, proper_count=proper_count, bad_count=bad_count)
         else:
-            return render_template("NLTK_Senti.html", data=data, sentiment=sentiment)
+            return render_template("NLTK_Senti.html", data=data, sentiment=sentiment,
+                                   article_count=article_count, proper_count=proper_count, bad_count=bad_count)
     else:
         return render_template("splash.html")
 
--- get_news.py
+++ get_news.py
@@ -18,26 +18,13 @@ def sentiment_summary_tb(data_1):
     score = [round(TextBlob(article).sentiment.polarity, 4) for article in data_1]
     return score
 
-'''
-def sentiment_summary_nltk(data_1):
-    sia = SentimentIntensityAnalyzer()
-    score = ["Positive" if sia.polarity_scores(article)["compound"] >= 0.1 else ("Negative" if sia.polarity_scores(article)["compound"] <= -0.1 else "Neutral") for article in data_1]
-    return score
 
-
-def sentiment_summary_tb(data_1):
-    score = ["Positive" if TextBlob(article).sentiment.polarity >= 0.1
-             else ("Negative" if TextBlob(article).sentiment.polarity <= -0.1 else "Neutral") for article in data_1]
-    return score
-'''
-
-
-def bing_api_search(search_term):
+def bing_api_search(search_term, search_num):
     # Setup Request to Bing API and save JSON response
-    subscription_key = "XXXXXXXXXXXXXXXXXXXXXXX"
+    subscription_key = "e8dff46a821b4e45990f36ebd17f51f2"
     search_url = "https://api.bing.microsoft.com/v7.0/news/search"
     headers = {"Ocp-Apim-Subscription-Key": subscription_key}
-    params = {"category": "Business", "count": 5, "q": search_term, "textDecorations": True, "textFormat": "HTML"}
+    params = {"category": "Business", "count": search_num, "q": search_term, "textDecorations": True, "textFormat": "HTML"}
     response = requests.get(search_url, headers=headers, params=params)
     response.raise_for_status()
     bing_resp = response.json()["value"]
